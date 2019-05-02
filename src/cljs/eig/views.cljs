5f318b4847f42060a7c19bcaefb7f1c3bdf23aa3
Bastien <bzg@gnu.org>
2019-05-02 11:15:18 +0200

    Use a side menu

--- src/cljs/eig/views.cljs
+++ src/cljs/eig/views.cljs
@@ -39,20 +39,22 @@
   (case view-name
     :chart [display-chart]))
 
+(defn side-menu []
+  [ant/menu {:mode "inline" :theme "dark" :style {:height "100%"}}
+   [ant/menu-item "Menu Item"]
+   [ant/menu-sub-menu {:title "Sub Menu"}
+    [ant/menu-item "Item 1"]
+    [ant/menu-item "Item 2"]]
+   [ant/menu-item {:disabled true} "Menu with Icons"]
+   [ant/menu-item (r/as-element [:span [ant/icon {:type "home"}] "Menu Item"])]
+   [ant/menu-sub-menu {:title (r/as-element [:span [ant/icon {:type "setting"}] "Sub Menu"])}
+    [ant/menu-item (r/as-element [:span [ant/icon {:type "user"}] "Item 1"])]
+    [ant/menu-item (r/as-element [:span [ant/icon {:type "notification"}] "Item 2"])]]])
+
 (defn main-panel []
   [ant/locale-provider {:locale (ant/locales "fr_FR")}
    [ant/layout
-    [ant/layout-header
-     [ant/menu {:mode "horizontal" :theme "dark"}
-      [ant/menu-item "Menu without icons"]
-      [ant/menu-item "Menu Item"]
-      [ant/menu-sub-menu {:title "Sub Menu"}
-       [ant/menu-item "Item 1"]
-       [ant/menu-item "Item 2"]]]]
+    [ant/layout-sider [side-menu]]
     [ant/layout-content {:style {:padding "50px 50px" :width "85%" :margin "auto"}}
-     ;; [ant/breadcrumb
-     ;;  [ant/breadcrumb-item "Bonjour"]
-     ;;  [ant/breadcrumb-item "ReBonjour"]]
-     [layout-content-view @(re-frame/subscribe [::subs/view])]]
-    [ant/layout-footer
-     [:p "Pied de page."]]]])
+     [layout-content-view @(re-frame/subscribe [::subs/view])]
+     [ant/layout-footer [:p "Pied de page."]]]]])
